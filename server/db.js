a232697da111af5fd61bfaa419ecb3e0111034c2
Eyasu31260592 <betrueyassu355@gmail.com>
2025-07-14 00:54:03 +0300

    db problem fix

--- server/db.js
+++ server/db.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000 
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
